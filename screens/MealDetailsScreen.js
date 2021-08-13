39a19a3d7f1dd711f1d960f876925745eaa8b106
Norudah <pierucci.romain@gmail.com>
2021-08-13 14:59:55 +0200

    Structure of MealDetails

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,10 +1,60 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import { Colors, Spacing, Sizing } from "../style";
 
-export default function MealDetailsScreen() {
+export default function MealDetailsScreen({ navigation, route }) {
+  const { mealId } = route.params;
+  const meal = MEALS.find((meal) => meal.id === mealId);
+
+  const ingredients = meal.ingredients.map((ingredient) => <Text>{ingredient}</Text>);
+  const steps = meal.steps.map((step) => <Text>{step}</Text>);
+
   return (
-    <View>
-      <Text>Meals Details</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image style={styles.image} source={{ uri: meal.imageUrl }} />
+      <View style={styles.information}>
+        <View style={styles.informationLabelContainer}>
+          <Text style={styles.informationLabel}>{meal.duration} min</Text>
+        </View>
+        <View style={styles.informationLabelContainer}>
+          <Text style={styles.informationLabel}>{meal.complexity.toUpperCase()}</Text>
+        </View>
+        <View style={styles.informationLabelContainer}>
+          <Text style={styles.informationLabel}>{meal.affordability.toUpperCase()}</Text>
+        </View>
+      </View>
+      <View>
+        <Text>Ingredients</Text>
+        {ingredients}
+      </View>
+      <View>
+        <Text>Recette</Text>
+        {steps}
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: Sizing.x200,
+  },
+  information: {
+    flexDirection: "row",
+
+    width: "100%",
+  },
+  informationLabelContainer: {
+    flex: 1,
+  },
+  informationLabel: {
+    textAlign: "center",
+  },
+  ingredients: {},
+  recipe: {},
+});
--- style/sizing.js
+++ style/sizing.js
@@ -1,2 +1,3 @@
 export const x100 = 120;
 export const x110 = 140;
+export const x200 = 220;
