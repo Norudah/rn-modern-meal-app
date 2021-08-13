3f87e49bca000088915ffdb8865e07ee2723d26c
Norudah <pierucci.romain@gmail.com>
2021-08-13 15:53:53 +0200

    Add style to header + dl navigation-tab to package.json

--- App.js
+++ App.js
@@ -5,13 +5,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsScreen from "./screens/MealsScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import { Colors } from "./style";
+import { Button } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Categories">
+      <Stack.Navigator
+        initialRouteName="Categories"
+        screenOptions={{
+          headerTintColor: Colors.primary.brand,
+        }}
+      >
         <Stack.Screen
           name="Categories"
           component={CategoriesScreen}
@@ -25,7 +32,10 @@ export default function App() {
         <Stack.Screen
           name="MealDetails"
           component={MealDetailsScreen}
-          options={({ route }) => ({ title: route.params.mealName })}
+          options={({ route }) => ({
+            title: route.params.mealName,
+            headerRight: () => <Button title="favorite" />,
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import { Colors, Spacing, Sizing } from "../style";
 
