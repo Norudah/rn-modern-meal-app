4e96928f1dd5fb8125d31e5bffc3875d8c700f5f
Norudah <pierucci.romain@gmail.com>
2021-08-16 18:42:25 +0200

    Default text in case of empty meal

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -4,9 +4,21 @@ import { View, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 
 import MealsList from "../components/MealsList";
+import { Spacing } from "../style";
 
 export default function FavoritesScreen() {
   const favorites = useSelector((state) => state.favorites);
+
+  if (favorites.length === 0)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>No favorites found.</Text>
+        <Text style={styles.label}>
+          You can still add some with the little star in the top right corner of a meal
+          details sheet !
+        </Text>
+      </View>
+    );
 
   return <MealsList data={favorites} />;
 }
@@ -16,5 +28,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    padding: Spacing.padding.x30,
+  },
+  label: {
+    textAlign: "center",
+    marginBottom: Spacing.margin.x10,
   },
 });
